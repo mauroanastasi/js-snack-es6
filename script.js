5f8532f05f7a8f669572c0e38728139403d9f7a0
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-28 16:49:12 +0100

    [UPD] creo array di oggetti dove ogni oggetto descrive una bici

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Snack 1 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare in console la bici con peso minore.
+
+const bike = [
+    {
+        nome: `superquantum`,
+        peso: `35kg`
+    },
+    {
+        nome: `essential`,
+        peso: `35kg`
+    },
+    {
+        nome: `spiyceone`,
+        peso: `35kg`
+    },
+    {
+        nome: `bartella`,
+        peso: `35kg`
+    },
+    {
+        nome: `mountainfive`,
+        peso: `35kg`
+    },
+]
